de770f82a19733ceb01a975a8b87e541a03c7f00
navike21 <ivan@navike21.com>
2018-08-29 11:13:53 -0500

    subiendo archivos

--- data/email.php
+++ data/email.php
@@ -1,8 +1,12 @@
 <?php
 
     $nombres = utf8_decode($_POST["nombres"]);
-    $correo = $_POST["correo"];
-    $contenido = utf8_decode($_POST["contenido"]);
+    $correo = $_POST["email"];
+    $empresa = $_POST["empresa"];
+    $movil = $_POST["movil"];
+    $contenido = utf8_decode($_POST["mensaje"]);
+    // $email = $_POST["email"];
+    // $mensaje = $_POST[""];
 
     // $to = "ventas@grupoelide.pe";
     $to = "ivan@navike21.com";
@@ -16,6 +20,8 @@
     // compose message
     $message = "Hola, \n".$nombres." se ha contactado desde la web.\n\n";
     $message .= "Mensaje:\n";
+    $message .= utf8_decode("Empresa: ").$empresa."\n";
+    $message .= utf8_decode("Teléfono: ").$movil."\n";
     $message .= $contenido;
     // $message .= " Praesent ac augue sed enim aliquam auctor. Ut dignissim ultricies est.";
     // $message .= " Pellentesque convallis tempor tortor. Nullam nec purus.";
